c7fe70d226553ff9ff1964ff997d38b1f472e399
Harsh Patel <patelharsh9797@gmail.com>
2025-01-31 14:47:24 +0000

    database added

--- src/env.js
+++ src/env.js
@@ -8,6 +8,13 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    // SINGLESTORE_DB_URL: z.string().url(),
+    SINGLESTORE_USER: z.string(),
+    SINGLESTORE_PASS: z.string(),
+    SINGLESTORE_HOST: z.string(),
+    SINGLESTORE_PORT: z.string(),
+    SINGLESTORE_DB_NAME: z.string(),
+
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -29,6 +36,12 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    // SINGLESTORE_DB_URL: process.env.SINGLESTORE_DB_URL,
+    SINGLESTORE_USER: process.env.SINGLESTORE_USER,
+    SINGLESTORE_PASS: process.env.SINGLESTORE_PASS,
+    SINGLESTORE_HOST: process.env.SINGLESTORE_HOST,
+    SINGLESTORE_PORT: process.env.SINGLESTORE_PORT,
+    SINGLESTORE_DB_NAME: process.env.SINGLESTORE_DB_NAME,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
