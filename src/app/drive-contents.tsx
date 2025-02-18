ad82e0a761539846f0b8380fd57bfd924f5fb3e8
Harsh Patel <patelharsh9797@gmail.com>
2025-02-18 17:50:15 +0000

    allow all file types

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -63,7 +63,7 @@ export default function DriveContents(props: {
         </div>
 
         <UploadButton
-          endpoint="imageUploader"
+          endpoint="driveUploader"
           onClientUploadComplete={() => {
             navigate.refresh();
           }}
